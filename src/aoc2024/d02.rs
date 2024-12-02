1db3af8a0532e174771e2a7c25e6209b65385f98
Patrick Himmelmann <himmelmannp@gmail.com>
2024-12-02 19:13:39 +0100

    Day 2 part 2

--- src/aoc2024/d02.rs
+++ src/aoc2024/d02.rs
@@ -1,31 +1,47 @@
 use crate::AocInput;
 
+pub fn is_safe(data: &[i64]) -> bool {
+    let mut values = data.iter();
+    let mut current = values.next().unwrap();
+    let mut dir = None;
+    for v in values {
+        let delta = v - current;
+        if delta == 0 || delta.abs() > 3 {
+            return false;
+        }
+        let sign = delta.signum();
+        if *dir.get_or_insert(sign) != sign {
+            return false;
+        }
+        current = v;
+    }
+    true
+}
+
 pub fn f(input: AocInput) -> crate::AocResult {
     let mut res1 = 0;
+    let mut res2 = 0;
     for line in input.lines() {
         let line = line.unwrap();
-        let mut values = line
+        let values: Vec<_> = line
             .split_ascii_whitespace()
-            .map(|x| x.parse::<i64>().unwrap());
-        let mut current = values.next().unwrap();
-        let mut dir = None;
-        let mut is_safe = true;
-        for v in values {
-            let delta = v - current;
-            if delta == 0 || delta.abs() > 3 {
-                is_safe = false;
-                break;
-            }
-            let sign = delta.signum();
-            if *dir.get_or_insert(sign) != sign {
-                is_safe = false;
-                break;
-            }
-            current = v;
-        }
-        if is_safe {
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect();
+
+        if is_safe(&values) {
             res1 += 1;
+            res2 += 1;
+            continue;
+        }
+
+        for i in 0..values.len() {
+            let mut v2 = values.clone();
+            v2.remove(i);
+            if is_safe(&v2) {
+                res2 += 1;
+                break;
+            }
         }
     }
-    (res1).into()
+    (res1, res2).into()
 }
