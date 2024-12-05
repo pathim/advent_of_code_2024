44bbf213f99d3fbaa6cc6dc493a59d8f040d58f0
Patrick Himmelmann <himmelmannp@gmail.com>
2024-12-05 22:17:50 +0100

    Day 5 optimization

--- src/aoc2024/d05.rs
+++ src/aoc2024/d05.rs
@@ -1,37 +1,22 @@
 use std::{
-    cmp,
+    cmp::Ordering,
     collections::{HashMap, HashSet},
 };
 
 use crate::AocInput;
 
-fn check(rules: &HashMap<i64, HashSet<i64>>, data_map: &HashMap<i64, usize>) -> bool {
-    for (val, pos) in data_map {
-        if let Some(must_be_after) = rules.get(val) {
-            for after_val in must_be_after {
-                if let Some(after_pos) = data_map.get(after_val) {
-                    if pos > after_pos {
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-    true
-}
-
-fn compare(rules: &HashMap<i64, HashSet<i64>>, a: &i64, b: &i64) -> cmp::Ordering {
+fn compare(rules: &HashMap<i64, HashSet<i64>>, a: &i64, b: &i64) -> Ordering {
     if let Some(gt) = rules.get(a) {
         if gt.contains(b) {
-            return cmp::Ordering::Less;
+            return Ordering::Less;
         }
     }
     if let Some(lt) = rules.get(b) {
         if lt.contains(a) {
-            return cmp::Ordering::Greater;
+            return Ordering::Greater;
         }
     }
-    cmp::Ordering::Equal
+    Ordering::Equal
 }
 
 pub fn f(input: AocInput) -> crate::AocResult {
@@ -51,18 +36,17 @@ pub fn f(input: AocInput) -> crate::AocResult {
     }
     for line in lines {
         let line = line.unwrap();
-        let mut data_map = HashMap::new();
-        let mut data_vec = Vec::new();
-        for (i, val) in line.split(',').enumerate() {
-            let val: i64 = val.parse().unwrap();
-            data_map.insert(val, i);
-            data_vec.push(val);
-        }
-        if check(&rules, &data_map) {
-            res1 += data_vec[data_vec.len() / 2];
+        let data_vec = line
+            .split(',')
+            .map(|x| x.parse().unwrap())
+            .collect::<Vec<i64>>();
+        let mut sorted_data = data_vec.clone();
+        sorted_data.sort_unstable_by(|a, b| compare(&rules, a, b));
+        let middle_value = sorted_data[sorted_data.len() / 2];
+        if data_vec == sorted_data {
+            res1 += middle_value;
         } else {
-            data_vec.sort_unstable_by(|a, b| compare(&rules, a, b));
-            res2 += data_vec[data_vec.len() / 2];
+            res2 += middle_value;
         }
     }
     (res1, res2).into()
