2e02ef08c7f1d808410f7ea56d29c8b4379db2a5
Patrick Himmelmann <himmelmannp@gmail.com>
2024-12-07 03:03:07 +0100

    Day 6 part 2

--- src/aoc2024/d06.rs
+++ src/aoc2024/d06.rs
@@ -12,16 +12,16 @@ fn pos_within<T: Ord>(val: (T, T), lower: (T, T), upper: (T, T)) -> bool {
 
 pub fn f(input: AocInput) -> crate::AocResult {
     let mut res2 = 0;
-    let (grid, found) = input.to_2d_array_finding_chars(&['#', '^']);
+    let (grid, mut found) = input.to_2d_array_finding_chars(&['#', '^']);
     let height = grid.len();
     let width = grid.first().unwrap().len();
     let size = (height as isize, width as isize);
-    let start = found.get(&'^').unwrap().iter().next().unwrap();
-    let obstacles = found.get(&'#').unwrap();
+    let start = found.get(&'^').unwrap().iter().next().copied().unwrap();
+    let obstacles = found.get_mut(&'#').unwrap();
     let mut dir = (0, -1);
     let mut visited = HashSet::new();
-    visited.insert(*start);
-    let mut pos = *start;
+    let mut pos = start;
+    visited.insert(pos);
     while pos_within(pos, (0, 0), size) {
         let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
         if obstacles.contains(&new_pos) {
@@ -32,5 +32,30 @@ pub fn f(input: AocInput) -> crate::AocResult {
         visited.insert(pos);
     }
     visited.remove(&pos);
-    (visited.len(), res2).into()
+    let res1 = visited.len();
+    visited.remove(&start);
+
+    for new_obstacle_pos in visited {
+        pos = start;
+        dir = (0, -1);
+        let mut visited2 = HashSet::new();
+        visited2.insert((pos, dir));
+        obstacles.insert(new_obstacle_pos);
+        while pos_within(pos, (0, 0), size) {
+            let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
+            if obstacles.contains(&new_pos) {
+                dir = (-dir.1, dir.0);
+                continue;
+            }
+            pos = new_pos;
+
+            if !visited2.insert((pos, dir)) {
+                res2 += 1;
+                break;
+            }
+        }
+        obstacles.remove(&new_obstacle_pos);
+    }
+
+    (res1, res2).into()
 }
