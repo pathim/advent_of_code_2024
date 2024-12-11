d5c47ad89b441f090946e015c3fabcdbbde0975d
Patrick Himmelmann <himmelmannp@gmail.com>
2024-12-11 20:05:02 +0100

    Day 11 part 2

--- src/aoc2024/d11.rs
+++ src/aoc2024/d11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::AocInput;
 
 fn next_value(n: i64) -> Vec<i64> {
@@ -15,23 +17,34 @@ fn next_value(n: i64) -> Vec<i64> {
         }
     }
 }
-fn update(values: &[i64]) -> Vec<i64> {
-    values.iter().flat_map(|v| next_value(*v)).collect()
+
+fn count_after(val: i64, n: usize, memo: &mut HashMap<(i64, usize), usize>) -> usize {
+    if n == 0 {
+        1
+    } else if let Some(v) = memo.get(&(val, n)) {
+        *v
+    } else {
+        let v = next_value(val)
+            .into_iter()
+            .map(|v| count_after(v, n - 1, memo))
+            .sum();
+        memo.insert((val, n), v);
+        v
+    }
 }
 
 pub fn f(input: AocInput) -> crate::AocResult {
     let mut res2 = 0;
     let line = input.lines().next().unwrap().unwrap();
-    let mut values: Vec<_> = line
+    let values: Vec<_> = line
         .split_ascii_whitespace()
         .map(|x| x.parse::<i64>().unwrap())
         .collect();
 
-    for _ in 0..25 {
-        values = update(&values);
-    }
+    let mut memo = HashMap::new();
 
-    let res1 = values.len();
+    let res1: usize = values.iter().map(|v| count_after(*v, 25, &mut memo)).sum();
+    let res2: usize = values.iter().map(|v| count_after(*v, 75, &mut memo)).sum();
 
     (res1, res2).into()
 }
