8e2bd5f233213794c4ed08de3b553c253caad27b
Patrick Himmelmann <himmelmannp@gmail.com>
2024-12-23 10:59:50 +0100

    day 22 part 2

--- src/aoc2024/d22.rs
+++ src/aoc2024/d22.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use crate::{AocInput, AocResult};
 
 struct Secret {
@@ -27,7 +29,7 @@ impl Iterator for Secret {
 
 pub fn f(input: AocInput) -> AocResult {
     let mut res1 = 0;
-    let mut prices = Vec::new();
+    let mut total_money = HashMap::new();
     for s in input
         .lines()
         .map(|l| l.unwrap().parse::<u64>().unwrap())
@@ -35,13 +37,27 @@ pub fn f(input: AocInput) -> AocResult {
     {
         let values = s.take(2000).collect::<Vec<_>>();
         res1 += values.last().unwrap();
-        prices.push(
-            values
-                .into_iter()
-                .map(|v| (v % 10) as i8)
-                .collect::<Vec<_>>(),
-        );
+        let current_prices = values
+            .into_iter()
+            .map(|v| (v % 10) as i64)
+            .collect::<Vec<_>>();
+        let current_deltas = current_prices
+            .windows(2)
+            .map(|x| x[1] - x[0])
+            .collect::<Vec<_>>();
+        let mut already_used = HashSet::new();
+        for (i, d) in current_deltas.windows(4).enumerate() {
+            if already_used.contains(d) {
+                continue;
+            }
+            let d = d.to_owned();
+            let money = total_money.entry(d.clone()).or_insert(0);
+            *money += current_prices[i + 4];
+            already_used.insert(d);
+        }
     }
 
-    res1.into()
+    let res2 = total_money.iter().map(|x| x.1).max().unwrap();
+
+    (res1, res2).into()
 }
